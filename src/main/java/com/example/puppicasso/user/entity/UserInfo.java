2f3aa5a0a3c0d621da5e503e539860c0f87ae171
Hoseok Seong <shsshs0125@naver.com>
2024-05-21 23:03:34 +0900

    UserInfo Entity

--- src/main/java/com/example/puppicasso/user/entity/UserInfo.java
+++ src/main/java/com/example/puppicasso/user/entity/UserInfo.java
@@ -23,17 +23,17 @@ public class UserInfo extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true, nullable = false)
-    private String username;
-    @Column(nullable = false)
-    private String password;
-    @Enumerated(EnumType.STRING)
-    private Role role;
+    private Long userId;
+    private String profile_pic;
+    private String grade;
+    private Long subscriptionId;
 
     @Builder
-    public UserInfo(Long id, String username, String password) {
+    public UserInfo(Long id, Long userId, String profile_pic, String grade, Long subscriptionId) {
         this.id = id;
-        this.username = username;
-        this.password = password;
-        this.role = Role.USER;
+        this.userId = userId;
+        this.profile_pic = profile_pic;
+        this.grade = grade;
+        this.subscriptionId = subscriptionId;
     }
 }
