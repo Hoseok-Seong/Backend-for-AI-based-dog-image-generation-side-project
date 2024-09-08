390c01c51aa7f9cef37715432afaa719af442f11
Hoseok Seong <shsshs0125@naver.com>
2024-09-09 00:26:43 +0900

    ENUM 감정표현 추가

--- src/main/java/com/example/puppicasso/domain/ai/prompt/DogAttributes.java
+++ src/main/java/com/example/puppicasso/domain/ai/prompt/DogAttributes.java
@@ -47,6 +47,8 @@ public enum DogAttributes {
 
     // Expression
     EXPRESSION_HAPPY("Happy", "행복한"),
+    EXPRESSION_CURIOUS("Curious", "호기심 많은"),
+    EXPRESSION_PLAYFUL("Playful", "장난스러운"),
     EXPRESSION_CHILL("Chill", "여유로운"),
     EXPRESSION_RELAXED("Relaxed", "편안한"),
     EXPRESSION_EXCITED("Excited", "신난"),
@@ -54,6 +56,8 @@ public enum DogAttributes {
     EXPRESSION_SCARED("Scared", "무서워하는"),
     EXPRESSION_BORED("Bored", "지루한"),
     EXPRESSION_SAD("Sad", "슬픈"),
+    EXPRESSION_TIRED("Tired", "피곤한"),
+    EXPRESSION_ANGRY("Angry", "화난"),
     EXPRESSION_ALERT("Alert", "경계하는"),
 
     // Pose
